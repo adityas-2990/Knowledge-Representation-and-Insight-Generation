9605ba41281195c2694ed3f1d7d8cd0f6a855044
Aditya <adi.official.2990@gmail.com>
2024-06-06 14:32:41 +0530

    Made streamlit UI for the project

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import os
+import sys
+
+
+def main():
+    st.title("Talk to your Data")
+    uploaded_file = st.sidebar.file_uploader("Upload CSV", type="csv")
+
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        st.write(df)
+
+    query = st.text_input("Enter your query")
+
+
+
+
+if __name__ == "__main__":
+    main()
+
